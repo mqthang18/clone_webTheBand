4dce7ec47379bd962f1f885400b7b0fb7218a347
mqthang_18sh <maiquocthangt@gmail.com>
2022-11-26 16:39:28 +0700

    Update slide

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 Clone web The Band - Basic website programming course
+
+Source ref: 
+https://www.w3schools.com/w3css/tryw3css_templates_band.htm
--- assets/js/script.js
+++ assets/js/script.js
@@ -3,8 +3,8 @@
 
 var eles = document.getElementsByClassName('res');
 
-console.log(eles)
-console.log(eles.length)
+// console.log(eles)
+// console.log(eles.length)
 function OpenNavigation() {
     for (let i = 0; i < eles.length; i++) {
         // console.log(eles[i].getAttribute('class'))
@@ -21,3 +21,31 @@ function OpenNavigation() {
         }
     }
 }
+
+var slides = document.getElementsByClassName('wrapbanner');
+
+function SliderChange(slides, j) {
+    // console.log(2)
+    var slides = document.getElementsByClassName('wrapbanner');
+    for (let i = 0; i < slides.length; i++) {
+        if (i != j) {
+            slides[i].setAttribute('class', 'wrapbanner');
+        } else {
+            slides[i].setAttribute('class', 'wrapbanner show');
+        }
+    }
+}
+
+var j = 0;
+
+setInterval(function() {
+
+    if (j < 2) {
+        SliderChange(slides, j)
+        j++;
+    }
+    else {
+        SliderChange(slides, j);
+        j = 0
+    }
+}, 2000)
